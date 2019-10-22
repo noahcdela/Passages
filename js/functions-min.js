f25da9ca2f6b508848ebe6170efd3625d1996aaf
Noah Delagardelle <noahcdela@gmail.com>
2019-10-22 17:30:32 -0500

    Mobile portrait and desktop completed

--- js/functions-min.js
+++ js/functions-min.js
@@ -4,7 +4,13 @@ $(function () {
 		scrollPercent = 200,
 		titleArrow = $('.title_arrow'),
 		titleLetter = $('.title_letter'),
-		img = $('.img');
+		img = $('.img'),
+		introtl = new TimelineLite(),
+		sectionIntro = $('section--intro'),
+		daggerTop = $('.dagger-top'),
+		daggerBottom = $('.dagger-bottom'),
+		lineLeft = $('.line--left'),
+		lineRight = $('.line--right');
 		
 		// The lower the number the more extreme the effect
 		parallaxPercent = 10;
@@ -29,10 +35,25 @@ $(function () {
 		});
 	});
 
-	TweenMax.staggerFrom(titleLetter, 2, {
+	introtl.from(daggerTop, 3, {
+		opacity: 0,
+		y: -300,
+		ease: Power2.easeInOut
+	})
+	.from(daggerBottom, 3.2, {
+		opacity: 0,
+		y: 300,
+		ease: Power1.easeInOut
+	}, '-=3')
+	.from(lineLeft, 1, {
+		width: 0
+	}, '-=2')
+	.from(lineRight, 1, {
+		width: 0
+	}, '-=2')
+	.staggerFrom(titleLetter, 2, {
 		x: 50,
 		opacity: 0,
 		ease: Power1.easeInOut
-	}, .2);
-
+	}, .2, '-=3.1')
 });
